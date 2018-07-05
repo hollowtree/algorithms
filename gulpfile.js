5f19fb5e809e7fa8366be4b0096c079e77a3a701
hollowtree <wangjianshu1994@gmail.com>
2018-07-05 22:54:04 +0800

    quick sort temp

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,15 @@ let gulp = require('gulp');
 
 let reload = browserSync.reload;
 
-let rootPath = __dirname + '/';
 
 gulp.task('serve', () => {
     browserSync({
-        server: {
-            baseDir: './'
-        }
+        server: true
     });
-    gulp.watch(['*.*'], reload);
+    gulp.watch(['**/*'], reload);
 });
 
-// gulp.task('buildLess', () => {
-//     gulp.src(rootPath+'less/*.less')
-//         .pipe(less())
-//         .pipe(gulp.dest(rootPath + 'css'));
-// });
-
-// gulp.task('watchFiles', () => {
-//     gulp.watch(rootPath + 'less/*.less', ['buildLess'])
-
-// });
 
 gulp.task('default', [
     'serve',
-    // 'watchFiles'
 ]);
